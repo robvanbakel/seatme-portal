9dc8e4eb93397ccfd1a20936e38c48c2c5218177
Rob van Bakel <robbakel@gmail.com>
2023-11-21 22:54:00 +0100

    Update main layout styling

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,8 +5,11 @@ defineProps<{
 </script>
 
 <template>
-  <div class="mb-6">
+  <div class="mb-6 flex h-12 items-center justify-between">
     <h1 class="text-4xl font-bold">{{ title }}</h1>
+    <slot name="headerActions"></slot>
   </div>
-  <slot></slot>
+  <div class="rounded-2xl bg-white p-8">
+    <slot></slot>
+  </div>
 </template>
