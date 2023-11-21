5a7500b25b7998d322d0812be0e5be154b14baf5
Rob van Bakel <robbakel@gmail.com>
2023-11-21 21:47:41 +0100

    Add main menu

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+import MainNavigationVue from "@/components/mainMenu/MainNavigation.vue";
+import RestaurantDetails from "@/components/mainMenu/RestaurantDetails.vue";
+</script>
+
 <template>
-  <RouterView />
+  <div class="flex">
+    <nav class="min-h-screen w-72 shrink-0 bg-white px-6">
+      <RestaurantDetails class="py-12" />
+      <MainNavigationVue />
+    </nav>
+    <main class="grow px-6 pt-12">
+      <RouterView />
+    </main>
+  </div>
 </template>
---
+++ src/components/mainMenu/MainNavigation.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import {
+  HomeIcon,
+  CalendarIcon,
+  Cog6ToothIcon,
+} from "@heroicons/vue/24/outline";
+import type { Component } from "vue";
+import type { RouteLocationRaw } from "vue-router";
+
+const menuItems: {
+  label: string;
+  icon: Component;
+  route: RouteLocationRaw;
+}[] = [
+  {
+    label: "Dashboard",
+    icon: HomeIcon,
+    route: { name: "dashboard" },
+  },
+  {
+    label: "Reservations",
+    icon: CalendarIcon,
+    route: { name: "reservations" },
+  },
+  {
+    label: "Settings",
+    icon: Cog6ToothIcon,
+    route: { name: "settings" },
+  },
+];
+</script>
+
+<template>
+  <div class="space-y-2">
+    <router-link
+      v-for="item in menuItems"
+      :key="item.label"
+      :to="item.route"
+      role="button"
+      class="flex items-center rounded-2xl p-5 text-slate-400 active:bg-slate-100 hover:[&:not(.bg-slate-100)]:bg-slate-50"
+      active-class="bg-slate-100 text-indigo-600"
+    >
+      <component :is="item.icon" class="mr-5 w-6" />
+      {{ item.label }}
+    </router-link>
+  </div>
+</template>
