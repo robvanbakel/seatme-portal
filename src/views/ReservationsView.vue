2b4d8134fc0776e3a631042a80f3b15b999c6aa9
Rob van Bakel <robbakel@gmail.com>
2023-11-22 12:05:33 +0100

    Add reservations header action

--- src/views/ReservationsView.vue
+++ src/views/ReservationsView.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
 import MainLayout from "@/layouts/MainLayout.vue";
+import { PlusIcon } from "@heroicons/vue/20/solid";
+import MainButton from "@/components/MainButton.vue";
 </script>
 
 <template>
-  <main-layout title="Reservations"> </main-layout>
+  <main-layout title="Reservations">
+    <template #headerActions>
+      <MainButton label="New reservation" :icon="PlusIcon" />
+    </template>
+  </main-layout>
 </template>
