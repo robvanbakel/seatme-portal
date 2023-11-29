7dfd6cfbdd28b1a003cc0b2d28c513b1e50e8754
Rob van Bakel <robbakel@gmail.com>
2023-11-29 18:16:29 +0100

    Add priority and variant props to main button

--- src/components/MainButton.vue
+++ src/components/MainButton.vue
@@ -1,15 +1,46 @@
 <script setup lang="ts">
-import type { Component } from "vue";
+import { computed, type Component } from "vue";
 
-defineProps<{
-  label: string;
-  icon?: Component;
-}>();
+const props = withDefaults(
+  defineProps<{
+    label: string;
+    icon?: Component;
+    priority?: "primary" | "secondary";
+    variant?: "default" | "block";
+  }>(),
+  {
+    priority: "primary",
+    variant: "default",
+  }
+);
+
+const priorityClasses = computed(() => {
+  switch (props.priority) {
+    case "primary":
+      return "bg-indigo-600 text-white hover:bg-indigo-700 active:bg-indigo-800";
+    case "secondary":
+      return "bg-slate-200 text-slate-800 hover:bg-slate-300 active:bg-slate-400";
+    default:
+      throw new Error("Invalid priority provided");
+  }
+});
+
+const variantClasses = computed(() => {
+  switch (props.variant) {
+    case "default":
+      return "rounded-2xl";
+    case "block":
+      return "rounded-md";
+    default:
+      throw new Error("Invalid variant provided");
+  }
+});
 </script>
 
 <template>
   <button
-    class="flex h-12 items-center whitespace-nowrap rounded-2xl bg-indigo-600 px-5 text-sm tracking-wide text-white hover:bg-indigo-700 active:bg-indigo-800"
+    class="flex h-12 items-center justify-center whitespace-nowrap px-5 text-sm tracking-wide"
+    :class="[priorityClasses, variantClasses]"
   >
     <component :is="icon" v-if="!!icon" class="-ml-1 mr-2 w-5" /> {{ label }}
   </button>
