0e750b3c68d5e7f6f3bb0d9b0cf101881a0af870
Rob van Bakel <robbakel@gmail.com>
2024-05-11 11:32:47 +0200

    Add responsive layout to setting blocks

--- components/SettingBlocks/BaseBlock.vue
+++ components/SettingBlocks/BaseBlock.vue
@@ -6,12 +6,12 @@ defineProps<{
 </script>
 
 <template>
-  <div class="grid grid-cols-[320px_auto] gap-x-6 gap-y-2">
+  <div class="grid gap-x-6 gap-y-2 xl:grid-cols-[320px_auto]">
     <h4 class="col-span-full text-lg font-bold">{{ title }}</h4>
     <p class="text-sm text-slate-400">
       <slot name="description">{{ description }}</slot>
     </p>
-    <div>
+    <div class="max-xl:mt-6">
       <slot></slot>
     </div>
   </div>
